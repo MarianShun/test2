9ca7e281401364ec4593c61516a3268625ad8b4a
Marian Shun <marian.shun@gmail.com>
2018-07-24 21:15:39 +0300

    2

--- test_candidates.py
+++ test_candidates.py
@@ -1,7 +1,14 @@
 import pytest
-from candidates import ClassCandidatesAPI
-class TestsGetList:
+from candidates import CandidatesAPI
+name = 'Marian'
+position = 'QA'
+class TestCandidatesAPI:
     def test_get_candidate_status_code_list(self):
-        assert ClassCandidatesAPI().get_all_candidates().status_code == 200
-    def test_get_candidate_status_code_list(self):
-        assert ClassCandidatesAPI().get_all_candidates().json()
+        assert CandidatesAPI().get_all_candidates().status_code == 200
+    def test_get_candidate_json_list(self):
+        assert CandidatesAPI().get_all_candidates().json()
+    def test_candidate_status_code_get(self):
+        new = CandidatesAPI().create_candidate(name, position)
+        assert new.status_code == 201  # preconditions
+        id = new.json()['candidate']['id']
+        assert CandidatesAPI().get_candidate_by_id().status_code == 200
